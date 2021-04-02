1bb446684cdc944474ead8b3d1f4442d34eb48c2
aikoels <els.q@northeastern.edu>
2021-04-01 22:37:44 -0400

    Push For Presentation Tomorrow

--- analyzer.py
+++ analyzer.py
@@ -7,21 +7,22 @@ Northeastern University
 Contains the code used to analyze wav files and determine its genre.
 '''
 import csv
-from csv import reader
-
-import librosa.display
 import os
 import platform
 import pathlib
+from csv import reader
+import librosa.display
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import warnings
 
 # Remove warnings from matplotlib upgrade
 warnings.filterwarnings('ignore')
-
 # Librosa Configuration Values (Song Analysis Parameters)
 import numpy
+
+# Debug Toggle
+DEBUG = True
 
 sr = None  # Resampling rate for songs, set to None to disable
 x_axis = 'time'  # X Axis unit for graphs
@@ -29,7 +30,30 @@ y_axis = 'log'  # Y Axis unit for graphs, e.g. 'hz' or 'log'
 window_size = 1024
 hop_length = 512
 
-# Program Defaults
+# Constants for readability
+top_songs = "TOP_SONGS"
+most_matches = "MOST_MATCHES"
+name = "NAME"
+genre = "GENRE"
+spectral_rolloff = "SPECTRAL_ROLLOFF"
+spectral_centroid = "SPECTRAL_CENTROID"
+zero_crossing_rate = "ZERO_CROSSING_RATE"
+bpm = "BPM"
+
+# Set feature buffer amounts used for "most_matches" method
+feature_buffer = {
+    spectral_centroid: .05,
+    spectral_rolloff: .05,
+    zero_crossing_rate: .05,
+    bpm: .5
+}
+
+# Chose Comparison Method:
+#   most_matches: Select the genre with the most matches, using the feature_buffer values to determine what is a match
+#
+COMPARISON_METHOD = most_matches
+
+# System Defaults
 FILE_DELIM = None
 if platform.system() == 'Windows':
     FILE_DELIM = '\\'
@@ -47,6 +71,7 @@ def clean_output_directory():
         os.remove(os.path.join(OUTPUT_DIRECTORY, file))
 
 
+# Analyze Song & Write Features to Data File
 def analyze_song(song_name):
     print("Analyzing Song: " + song_name)
     global sr
@@ -67,40 +92,75 @@ def analyze_song(song_name):
     plt.clf()
 
     # === Extract Features ===
-    spectral_centroid = librosa.feature.spectral_centroid(x, sr=sr)
-    spectral_rolloff = librosa.feature.spectral_rolloff(x, sr=sr)
-    zero_crossing_rate = librosa.feature.zero_crossing_rate(x)
-    bpm = librosa.beat.tempo(x, sr=sr)[0]
+    song_spectral_centroid = librosa.feature.spectral_centroid(x, sr=sr)
+    song_spectral_rolloff = librosa.feature.spectral_rolloff(x, sr=sr)
+    song_zero_crossing_rate = librosa.feature.zero_crossing_rate(x)
+    song_bpm = librosa.beat.tempo(x, sr=sr)[0]
 
     # === Write Mean of Features to Data File ===
     file = open(OUTPUT_DIRECTORY + FILE_DELIM + 'features.csv', "a", newline='')
     with file:
         writer = csv.writer(file)
-        writer.writerow([song_name, numpy.mean(spectral_centroid),
-                         numpy.mean(spectral_rolloff), numpy.mean(zero_crossing_rate), bpm])
+        if DEBUG: print("Spectral Centroid: %f\n"
+                        "Spectral Rolloff: %f\n"
+                        "Zero Crossing: %f\n"
+                        "BPM: %i" % (numpy.mean(
+                                         song_spectral_centroid),
+                                     numpy.mean(
+                                         song_spectral_rolloff),
+                                     numpy.mean(
+                                         song_zero_crossing_rate),
+                                     song_bpm), end='\n\n')
+        writer.writerow([song_name, numpy.mean(song_spectral_centroid),
+                         numpy.mean(song_spectral_rolloff), numpy.mean(song_zero_crossing_rate), song_bpm])
 
-    '''
-    Strategy One:
-        - Select "closeness buffer" (value for each feature it can be within to match)
-            Examples:
-                Spectral Centroid - 5%
-                Spectral RollOff - 10%
-                "Swing" - 3%
-        - Iterate through songs, collecting matches
-        - Go through matches, determine which genre(s) the most matches come from
-        - Experiment with different "closeness buffers"
-    Strategy Two:
-        - Iterate through database, finding top X matches
-        - See which genre(s) the top X matches are from
-        - Experiment by changing X
-    Experiment By Comparing Strategy One & Two
-    '''
-    # Open Song Database
-    database = open(DATA_DIRECTORY + FILE_DELIM + 'mean_features' + FILE_DELIM + 'features.csv', 'r')
-    with database as database_file:
-        csv_reader = reader(database_file)
-        for song in csv_reader:
-            print(song)
+
+def match_songs():
+    # === Read Song Features from Data File ===
+    file = open(OUTPUT_DIRECTORY + FILE_DELIM + 'features.csv', "r")
+    with file:
+        csv_reader = csv.reader(file)
+        song_data = list(csv_reader)
+
+    for song in song_data:
+        matches = {}
+        # Open Song Database
+        database_file = open(DATA_DIRECTORY + FILE_DELIM + 'mean_features' + FILE_DELIM + 'features.csv', 'r')
+        with database_file:
+            csv_reader = csv.reader(database_file)
+            database_list = list(csv_reader)
+            for data_song in database_list:
+                if song_match(song, data_song):
+                    # If already counting this genre, add one
+                    if data_song[0] in matches.keys():
+                        matches[data_song[0]] += 1
+                    else:  # Otherwise, set the count to 1
+                        matches[data_song[0]] = 1
+        print("Song Name: %s\nSong Genres:" % song[0], end=' ')
+        genres = sorted(matches, key=matches.get)
+        genres.reverse()
+        print(*genres[:2], sep=', ')
+        if DEBUG: print("All Matches:", matches)
+        print()
+
+
+# Return true if a and b are within (r/100)% of each other
+def within(a, b, r):
+    return (float(1.0) - float(r)) * float(a) <= float(b) <= (float(1.0) + float(r)) * float(a)
+
+
+# Returns true if the song is a match in features
+def song_match(song_list, data_list):
+    data = {name: data_list[0], genre: data_list[1], spectral_centroid: data_list[2], spectral_rolloff: data_list[3],
+            zero_crossing_rate: data_list[4], bpm: data_list[5]}
+
+    song = {name: song_list[0], spectral_centroid: song_list[1], spectral_rolloff: song_list[2],
+            zero_crossing_rate: song_list[3], bpm: song_list[4]}
+
+    return (within(song[spectral_centroid], data[spectral_centroid], feature_buffer[spectral_centroid]) and
+            within(song[spectral_rolloff], data[spectral_rolloff], feature_buffer[spectral_rolloff]) and
+            within(song[zero_crossing_rate], data[zero_crossing_rate], feature_buffer[zero_crossing_rate]) and
+            within(song[bpm], data[bpm], feature_buffer[bpm]))
 
 
 clean_output_directory()
@@ -109,3 +169,4 @@ for filename in os.listdir(INPUT_DIRECTORY):
         analyze_song(filename)
     else:
         continue
+match_songs()
